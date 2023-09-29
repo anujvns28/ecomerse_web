caf1d47fc71243fe27a64f8fd502db4cc368a435
anujYadav <anujvns28@gmail.com>
2023-09-29 23:19:18 +0530

    category or subCategoryid add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 
 const {dbConnect} = require("./config/database");
 const userRoutes = require("./routes/user");
+const productRoutes = require("./routes/product")
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000
@@ -12,6 +13,7 @@ app.use(express.json());
 
 //mounging
 app.use("/api/v1/user",userRoutes);
+app.use("/api/v1/product",productRoutes);
 app.listen(PORT , () =>{
     console.log("server started successfully")
 })
