5cc67043a9ca317dddaf1aa994ed3ab537bc6f31
anujYadav <anujvns28@gmail.com>
2024-06-08 23:38:56 +0530

    updation again

--- routes/payment.js
+++ routes/payment.js
@@ -5,6 +5,8 @@ const {capturePayment, verifyPayment, } = require("../controllers/Payment");
 
 
 router.post("/capturePayment",capturePayment);
+
+
 router.post("/verifyPayment",verifyPayment);
 
 module.exports = router;
